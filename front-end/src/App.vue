43917ca04467333c6bcf25c0b7bc7db01d5df4ab
jonath8 <jonathan.brent.nelson@gmail.com>
2021-12-04 14:32:40 -0700

    Got base code

---
+++ front-end/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+<div id="app">
+  <div class="header">
+    <router-link to="/">
+      <div class="logo">
+        <img src="/ordinary.png">
+      </div>
+    </router-link>
+    <div class="title">
+      <h1>Museum of Ordinary Objects</h1>
+    </div>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <div class="footer">
+    <router-link to="/admin">Admin</router-link>
+    <p class="footer">My Project's Github: <a href="https://github.com/BYUCS260/museum-jonath8">LINK</a></p>
+  </div>
+</div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  background: #fff;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header */
+.header {
+  display: flex;
+  padding: 10px 100px 0px 100px;
+  background-color: #5BDEFF;
+  color: #1C454F;
+}
+
+.title {
+  margin-top: 5px;
+}
+
+.title h1 {
+  font-size: 30px;
+}
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+
+/* Footer */
+.footer {
+  height: 50px;
+  padding: 20px 100px 0px 100px;
+  background: #e3e3e3;
+  font-size: 12px;
+}
+
+.footer a {
+  color: #000;
+}
+
+h1 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 14px;
+}
+
+.footer {
+  margin: 0px;
+  padding-top: 0px;
+  padding-bottom: 25px;
+  text-align: center;
+}
+</style>
